b75377d4d82739a3451f0d928e35a80b0b75140a
0LE6 <135649528+0LE6@users.noreply.github.com>
2026-04-09 22:03:19 +0200

    feat: add general aliases

---
+++ bash/.shell/aliases/general.sh
@@ -0,0 +1,14 @@
+# default
+alias ls='ls --color=auto'
+alias grep='grep --color=auto'
+PS1='[\u@\h \W]\$ '
+
+# custom
+alias cls='clear'
+alias xz='exit'
+alias l='ls -AF'
+alias ll='ls -lahF'
+alias h='history'
+alias ff='fastfetch'
+alias ..='cd ..'
+alias ...='cd .. && cd ..'
