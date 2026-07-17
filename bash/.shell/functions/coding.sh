59606a706ee70f0c3e249536b92b4307ca74f080
0LE6 <135649528+0LE6@users.noreply.github.com>
2026-07-17 23:06:44 +0200

    chore: update functionaly of rcpp function alias

--- bash/.shell/functions/coding.sh
+++ bash/.shell/functions/coding.sh
@@ -1,9 +1,10 @@
-# g++: compile and run all in one 
+# g++: compile and run all in one
 rcpp() {
-    if [ $# -eq 1 ]; then
+    if [ $# > 1 ]; then
         g++ "$@" -o "${@%.*}" && \
         "./${@%.*}"
     else
         echo "Usage: rcpp <filename>.cpp"
     fi
 }
+
