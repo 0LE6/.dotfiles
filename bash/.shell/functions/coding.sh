448d59928335c579dd83772a42627b387f1044fd
0LE6 <135649528+0LE6@users.noreply.github.com>
2026-07-17 23:40:33 +0200

    feat: update rcpp alias funciton

--- bash/.shell/functions/coding.sh
+++ bash/.shell/functions/coding.sh
@@ -4,9 +4,14 @@ rcpp() {
         g++ "$@" -o "${@%.*}" && \
         "./${@%.*}"
     else
-        echo -e "Usage: rcpp <filename>.cpp\n"
+        echo -e "Usage: rcpp <flags> <filename>.cpp\n"
         echo -e "  Flags:\n"
         echo -e "      - Enable Warnings"
-        echo -e "          Wall -Weffc++ -Wextra -Wconversion -Wsign-conversionv"
+        echo -e "          Wall -Weffc++ -Wextra -Wconversion -Wsign-conversionv\n"
+        echo -e "      - Setting lang standard:"
+        echo -e "          -std=c++11, -std=c++14, -std=c++17, -std=c++20, or -std=c++23\n" 
     fi
+
+
 }
+
