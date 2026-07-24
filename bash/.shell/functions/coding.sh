cd83ee6f1bdf3be03d46b10b6c95502539e37919
0LE6 <135649528+0LE6@users.noreply.github.com>
2026-07-24 22:57:06 +0200

    feat: update run c plus cpus function

--- bash/.shell/functions/coding.sh
+++ bash/.shell/functions/coding.sh
@@ -1,17 +1,18 @@
 # g++: compile and run all in one 
 rcpp() {
     if [ $# -gt 0 ]; then
-        g++ "$@" -o "${@%.*}" && \
-        "./${@%.*}"
+        # g++ "$@" -o "${@%.*}" && \
+        # "./${@%.*}"
+        source="${!#}"   
+        output="${source%.*}"
+
+        g++ "$@" -o "$output" && "./$output"
     else
         echo -e "Usage: rcpp <flags> <filename>.cpp\n"
         echo -e "  Flags:\n"
         echo -e "      - Enable Warnings"
-        echo -e "          Wall -Weffc++ -Wextra -Wconversion -Wsign-conversionv\n"
+        echo -e "          -Wall -Weffc++ -Wextra -Wconversion -Wsign-conversionv\n"
         echo -e "      - Setting lang standard:"
         echo -e "          -std=c++11, -std=c++14, -std=c++17, -std=c++20, or -std=c++23\n" 
     fi
-
-
 }
-
